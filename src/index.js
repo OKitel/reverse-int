7c64346b8057319e78a69e751caa78c04b7e9b77
OKitel <kitelolga@gmail.com>
2021-03-11 18:42:19 +0100

    feat: solve task code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let result = n.toString().split('');
+    if ( result[0] == '-') {
+        result.shift()
+    }   
+    return +result.reverse().join(''); 
 }
